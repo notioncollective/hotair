582333454d6639ff7c60725c9a7daf8dba34ea23
Andy Dayton <amdayton@gmail.com>
2012-09-23 12:48:59 -0400

    Cleanup and document HA.twitter

--- public/js/modules/HA.twitter.js
+++ public/js/modules/HA.twitter.js
@@ -1,4 +1,7 @@
-// HA.twitter namespace
+/**
+ * The twitter module grabs twitted data from the server endpoint and stores it in memory.
+ * @class twitter
+ */
 HA.twitter = function(ns, $, _, C) {
 	
 	// PRIVATE
@@ -22,17 +25,6 @@ HA.twitter = function(ns, $, _, C) {
 		options = options || {};
 		_.extend(_options, options, _defaults);
 		_loadTweets({ startKey: 0, limit: _options.tweet_limit });
-		
-		// var test = "test" in _options ? true : false;
-		// if(test === false) {
-		// 	console.log("test is null");
-		// 	_load(_options.user, _options.d_list, 10, _page, {party:"d"});
-		// 	_load(_options.user, _options.r_list, 10, _page, {party:"r"});
-		// } else if(test === true) {
-		// 	console.log("loading test data");
-		// 	_handleLoad(REPUBLICANS_SAMPLE, {party: "r"});
-		// 	_handleLoad(DEMOCRATS_SAMPLE, {party: "d"});
-		// }
 	}
 	
 	/**
@@ -52,34 +44,9 @@ HA.twitter = function(ns, $, _, C) {
 		$.getJSON(uri, params)
 			.done(_handleLoadTweets)
 			.fail(function(){ throw "Error loading tweet data."; });
-		
-		// var uri='http://'+_options.uri+'/'+l+'?callback=?';
-		// console.log(uri);
-		// $.getJSON('http://'++, function(r) {
-		// 	console.log("Twitter loaded");
-		// 	// maybe change to use the "apply" method to manage scoping
-		// 	_handleLoad(r, o, that);
-		// });
 	}
 	
 	
-	/**
-    Load twitter data.
-		@private
-    @method _load
-		@param {String} u endpoint uri _(not currently used!)_.
-		@param {String} l Twitter list name.
-		@param {String} c callback _(not currently used!)_.
-		@param {Number} p page?? _(not currently used!)_
-		@param {Object} o options (currently used to set party -- `{party: 'r'}`)
-   */
-	// function _loadTwitterList(list) {
-	// 	var uri = _options.protocol;
-	// 	uri += _options.domain;
-	// 	uri += list
-	// 	$.getJSON()
-	// }
-
 
 	/**
     Handle loaded twitter data
@@ -97,32 +64,6 @@ HA.twitter = function(ns, $, _, C) {
 	}
 		
 	/**
-    Load twitter data.
-		@private
-    @method _load
-		@param {Object} r JSON response
-		@param {Object} o options (currently used to set party -- `{party: 'r'}`)
-		@param {Object} that Scoping variable _(shouldn't be necessary with module pattern)_
-   */
-	// r = response, o = extra data (party, so far), that = HA.twitter
-
-	// function _handleLoad(r, o, that) {
-	// 	console.log("Handling Load : "+o.party);
-	// 	
-	// 	_options[o.party] = _.map(r, function(tweet) {
-	// 		return {id: tweet.key, name: tweet.value.name, screen_name: tweet.value.screen_name, party: tweet.value.party, text: tweet.value.text};
-	// 	});
-	// 	if(_options.d.length > 0 && _options.r.length > 0) {
-	// 		// merge the two arrays
-	// 		_tweets = _.shuffle(_options.d.concat(_options.r));
-	// 		// self.callback(self.all);
-	// 		// Crafty.scene("gameplay");
-	// 		// Crafty.scene("start");
-	// 		return;
-	// 	}
-	// }
-	
-	/**
     See public method `getTweetSet'
 		@private
     @method _getTweetSet
@@ -130,11 +71,6 @@ HA.twitter = function(ns, $, _, C) {
 	function _getTweetSet(start, count) {
 		var end = start+count,
 				set = _tweets.slice(start, end);
-		// TODO: logic is needed to load new tweets when nearing the end
-		// 	r = _options.r.slice(start, end),
-		// 	d = _options.d.slice(start, end);
-		// var set = _.shuffle(r.concat(d));
-		// console.log("getTweetSet", set);
 		return set;
 	}
 		
