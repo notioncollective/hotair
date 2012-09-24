629f9a12c9c6fa4ea6aefbdffff5b5ea1795e0ed
Jonathan Wohl <jon@jonwohl.com>
2012-09-24 19:09:24 -0400

    fixed z-index issue on TweetDisplay component

--- public/js/components/tweet-display.js
+++ public/js/components/tweet-display.js
@@ -4,8 +4,9 @@ Crafty.c("TweetDisplay", {
 		this.addComponent("HTMLTemplate");
 		this.setTemplate($("#TweetDisplayTemplate").html());
 		this.w = Crafty.DOM.window.width;
+		this.z = 100;
 		
-		HA.m.subscribe(HA.e.ENEMY_SELECTED, this._handleEnemySelectedEvent)
+		HA.m.subscribe(HA.e.ENEMY_SELECTED, this._handleEnemySelectedEvent);
 	},
 	_handleEnemySelectedEvent: function(e, enemy) {
 		this.updateContent({text: enemy.tweet.text});
