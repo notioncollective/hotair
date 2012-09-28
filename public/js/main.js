5e60c8f78ce4bef2e51de44401592227d0717ff3
Jonathan Wohl <jon@jonwohl.com>
2012-09-28 17:31:00 -0400

    cleaned up main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,5 @@
 $(document).ready(function() {
 	
-	
-	
-	//HotAir namespace
-	/*HA = {
-		scoreUnit: 100,
-		enemyInterval: 5000,
-		party: "d",
-		game: null,
-		player: null,
-		score: 0,
-		level: 1
-	};*/
-	
-	console.log("Window width: "+Crafty.DOM.window.width);
-	console.log("Window height: "+Crafty.DOM.window.height);
-	
-	// Crafty.init();
-	// Crafty.scene("loading");
-	
 	HA.game.init();
-
-	// HA.startScreen = new Crafty.StartScreen();
-	// HA.startScreen.init();
 	
 });
