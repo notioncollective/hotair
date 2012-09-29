82b4775699cf501c92d86a16f6cb8c2233023e47
Andy Dayton <amdayton@gmail.com>
2012-09-29 03:15:05 -0400

    Got applescript part working

--- script/jsx/main.jsx
+++ script/jsx/main.jsx
@@ -2,24 +2,28 @@
 #include "PSD.utils.jsx"
 #include "PSD.layersetstofiles.jsx"
 
-function main(argv)
+function main(argv) {
 	if( BridgeTalk.appName === "photoshop" && app.documents.length > 0 ) {	
 		var opts = {
 			reverse_order: true
 		};
 		$.level = 1;
 		
-		try {
-			switch(argv[0]) {
-				// frames commend
-				case 'frames':
-					PSD.layersetstofiles.start(opts);
-					break;
-				default:
-					return "Please make a selection";
+		if(typeof argv === 'undefined' || argv.length === 'undefined' || argv.length < 1) {
+			return "Please make a selection";
+		} else {	
+			try {
+				switch(argv[0]) {
+					// frames commend
+					case 'frames':
+						PSD.layersetstofiles.start(opts);
+						break;
+					default:
+						return "Please make a valid selection";
+				}
+			} catch(err) {
+				return "Error: "+err.message;
 			}
-		} catch(err) {
-			return "Error: "+err.message;
 		}
 	} else {
 		return "Photoshop isn't even open, dummy!"
--- script/scpt/jsx.scpt
+++ script/scpt/jsx.scpt
@@ -1,6 +1,6 @@
 on run argv
-    tell application "Adobe Photoshop"
-        set js to "#include '../jsx/main.jsx';" & return
+    tell application "Adobe Photoshop CS5.1"
+        set js to "#include '/Users/amdayton/Code/hotairnode/script/jsx/main.jsx';" & return
         set js to js & "main(arguments);" & return
         do javascript js with arguments argv
     end tell
