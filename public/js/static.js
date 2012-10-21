b2ce5c3c390473b5416a67e7e55c449aaaac03d7
Andy Dayton <amdayton@gmail.com>
2012-10-21 18:21:37 -0400

    Client-side validation for contact form

--- public/js/static.js
+++ public/js/static.js
@@ -10,8 +10,53 @@ jQuery(function($){
 	}
 	
 	function _handleContactFormSubmit(e) {
-		var token = HA.getCsrfToken();
-		$(this).attr("disabled", true);				
+		var submitButton = $(this),
+				token = HA.getCsrfToken(),
+				email_re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+				valErrors = false;
+		
+		submitButton.attr("disabled", true);		
+		submitButton.html("Sending...");		
+
+				
+		$('#messages')
+			.html('')
+			.removeClass('error')
+			.removeClass('success')
+			.hide();
+			
+		console.log("fromName", $('#fromName').val().length);
+	
+		if(!_.isString($('#fromName').val() || $('#fromName').val().length < 1)) {
+			$('#messages')
+				.append('<li>You must include a name</li>')
+				.addClass('error');				
+			valErrors = true;
+		}
+		if(
+			!_.isString($('#fromEmail').val()
+			|| $('#fromEmail').val().length < 1)
+			|| !email_re.test($('#fromEmail').val())) {
+			$('#messages')
+				.append('<li>You must include a valid email</li>')
+				.addClass('error');
+			valErrors = true;	
+		}
+		if(!_.isString($('#emailBody').val() || $('#emailBody').val().length < 1)) {
+			$('#messages')
+				.append('<li>You didn\'t write a message!</li>')
+				.addClass('error');
+			valErrors = true;	
+		}
+
+		if(valErrors) {
+			console.log("validation errors detected");
+			$('#messages').show();
+			submitButton.removeAttr('disabled');
+			submitButton.html("Send!");		
+			return false;
+		}
+		
 		$.ajax({
 			url : "/contact/send",
 			type : "post",
@@ -35,6 +80,9 @@ jQuery(function($){
 					});
 					
 					$('#messages').show();
+					
+					$('#submit').removeAttr('disabled');
+					submitButton.html("Send!");		
 						
 				} else {
 					$('#messages')
@@ -46,6 +94,7 @@ jQuery(function($){
 					$('#fromEmail').val('');
 					$('#emailBody').val('');
 					$('#submit').removeAttr('disabled');
+					submitButton.html("Send!");		
 				} 	
 			}
 		});		
