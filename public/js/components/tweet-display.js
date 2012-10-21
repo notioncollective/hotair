29cabbc0c55f1bee968e5b72547a859bc3cd6c96
Andy Dayton <amdayton@gmail.com>
2012-10-21 16:16:02 -0400

    Remove tweetcolumn bs

--- public/js/components/tweet-display.js
+++ public/js/components/tweet-display.js
@@ -10,16 +10,16 @@ Crafty.c("TweetDisplay", {
 		this.footerSelector = "#TweetDisplayFooter";
 		this.enemy = null;
 		
-		console.log($('#TweetDisplayColumnTemplate').html());
+		// console.log($('#TweetDisplayColumnTemplate').html());
 		
-		this.tweetDisplayColumn = Crafty.e('HTMLTemplate').attr({
-			w:140, 
-			h:Crafty.DOM.window.height, 
-			y:0, 
-			x:-64, 
-			z:-999
-		});
-		this.tweetDisplayColumn.setTemplate($('#TweetDisplayColumnTemplate').html())
+		// this.tweetDisplayColumn = Crafty.e('HTMLTemplate').attr({
+			// w:140, 
+			// h:Crafty.DOM.window.height, 
+			// y:0, 
+			// x:-64, 
+			// z:-999
+		// });
+		// this.tweetDisplayColumn.setTemplate($('#TweetDisplayColumnTemplate').html())
 
 		this.w = Crafty.DOM.window.width;
 		this.z = 999;
@@ -27,26 +27,26 @@ Crafty.c("TweetDisplay", {
 		HA.m.subscribe(HA.e.PAUSE_GAME, this._hideTweetText);
 		HA.m.subscribe(HA.e.RESUME_GAME, this._showTweetText);
 		
-		this.bind('EnterFrame', function(){
-			if(this.enemy) {
-				var enemy_progress = (Crafty.DOM.window.height-this.enemy.y)/Crafty.DOM.window.height;
-				console.log("enemy balloon y: ", this.enemy.y);
-				console.log("enemy balloon progress: ", enemy_progress);
-				console.log("window height: ", Crafty.DOM.window.height);
-				$(this.arrowSelector).css('top', (60-66*enemy_progress)+'px');			
-			}
-		});
+		// this.bind('EnterFrame', function(){
+			// if(this.enemy) {
+				// var enemy_progress = (Crafty.DOM.window.height-this.enemy.y)/Crafty.DOM.window.height;
+				// console.log("enemy balloon y: ", this.enemy.y);
+				// console.log("enemy balloon progress: ", enemy_progress);
+				// console.log("window height: ", Crafty.DOM.window.height);
+				// $(this.arrowSelector).css('top', (60-66*enemy_progress)+'px');			
+			// }
+		// });
 		
 	},
 	_handleEnemySelectedEvent: function(e, enemy) {
 		this.enemy = enemy;
 		this.updateContent({text: enemy.tweet.text});
-		this.tweetDisplayColumn.updateContent();
-				
-		this.tweetDisplayColumn.x = Math.ceil(enemy.x)-10;
-		console.log("tweetDisplayColumn", this.tweetDisplayColumn)
-		$(this.columnSelector).css('background-position', '-'+this.tweetDisplayColumn.x+'px 0px');
-		$(this.footerSelector).css('background-position', '-'+this.tweetDisplayColumn.x+'px bottom');
+		// this.tweetDisplayColumn.updateContent();
+// 				
+		// this.tweetDisplayColumn.x = Math.ceil(enemy.x)-10;
+		// console.log("tweetDisplayColumn", this.tweetDisplayColumn)
+		// $(this.columnSelector).css('background-position', '-'+this.tweetDisplayColumn.x+'px 0px');
+		// $(this.footerSelector).css('background-position', '-'+this.tweetDisplayColumn.x+'px bottom');
 		// $(this.mainSelector).css('background-position', (enemy.x-14)+'px 0px');
 
 		// column
