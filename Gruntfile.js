6348e3101df188d74d8e660769d514ff5490d67c
Andy Dayton <amdayton@gmail.com>
2016-09-24 18:35:12 -0400

    Fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,19 @@ module.exports = function(grunt) {
 
 	  // minify & concat in one step
 	  uglify: {
-	  	options: {
-	  		banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-	  			'<%= grunt.template.today("yyyy-mm-dd") %> */' 
-	  	},
-	    files: {
-	      'public/js/dist/build.js': ['public/js/lib/*.js', 'public/js/modules/HA.js', 'public/js/modules/HA.*.js', 'public/js/components/*.js', 'public/js/scenes/*.js', 'public/js/main_prod.js'],
-	    }
+	  	main: {
+		  	options: {
+		  		banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+		  			'<%= grunt.template.today("yyyy-mm-dd") %> */' 
+		  	},
+		    files: {
+		      'public/js/dist/build.js': ['public/js/lib/*.js', 'public/js/modules/HA.js', 'public/js/modules/HA.*.js', 'public/js/components/*.js', 'public/js/scenes/*.js', 'public/js/main_prod.js'],
+		    }
+	  	}
 	  }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  	grunt.registerTask('default', 'uglify');
+  grunt.registerTask('default', 'uglify');
 };
